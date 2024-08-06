ad65acac4e6faf8a95ea997fe4f788634d28ef0d
Alex Burrows <burrows.alexj@gmail.com>
2024-08-06 15:09:36 -0400

    Simplify modal

--- script.js
+++ script.js
@@ -105,6 +105,7 @@ function createCard(book){
 function createAddButton(){
     // Create Add Button for library
     let addButton = document.createElement("button");
+    addButton.id = 'myBtn';
     bookGrid.appendChild(addButton);
     addButton.className = "book-card";
     addButton.style.color = "darkslategray";
@@ -134,42 +135,6 @@ function addBookToLibrary(book){
     bookGrid.appendChild(newCard);
 }
 
-function openModal(modal){
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
-}
-
-function closeModal(modal){
-    if (modal == null) return;
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-}
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget);
-        openModal(modal);
-    });
-});
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal');
-        closeModal(modal);
-    });
-});
-
-overlay.addEventListener('click', ()=>{
-    const modals = document.querySelectorAll('.modal.active');
-    modals.forEach(modal => {
-        closeModal(modal);
-    })
-})
 
 // Initialize 3 books for testing
 const book1 = new Book("Book 1", "Author 1", 1, true);
@@ -180,6 +145,32 @@ myLibrary.push(book2);
 myLibrary.push(book3);
 
 createAddButton();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 // Add the three books to the site
 for (let i = 0; i < myLibrary.length; i++){
